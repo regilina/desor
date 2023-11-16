bddd855ac03ae7ef3edf6076321ca0096a3280f4
Regi <gt12139@gmail.com>
2023-11-16 20:18:51 +0300

    Some changes

--- dezor/static/js/scripts.js
+++ dezor/static/js/scripts.js
@@ -1,15 +1,15 @@
 "use strict";
 let startTime = 0;
 function saveVisitTime() {
-    startTime = Math.floor(new Date().getTime() / 1000); // конвертируем время начала в секунды
+    startTime = Math.floor(new Date().getTime() / 1000);
     localStorage.setItem('visitTime', startTime.toString());
 }
 function sendVisitTimePeriodically() {
     setInterval(() => {
         const visitTime = localStorage.getItem('visitTime');
         if (visitTime) {
-            const currentTime = Math.floor(new Date().getTime() / 1000); // текущее время в секундах
-            const elapsedTime = currentTime - parseInt(visitTime); // вычисляем прошедшее время в секундах
+            const currentTime = Math.floor(new Date().getTime() / 1000);
+            const elapsedTime = currentTime - parseInt(visitTime);
             const requestData = {
                 id: localStorage.getItem('userId'),
                 data: {
@@ -18,7 +18,6 @@ function sendVisitTimePeriodically() {
             };
             const currentDomain = window.location.origin;
             const url = `${currentDomain}/submit_data/`;
-            // Отправляем данные на сервер
             fetch(url, {
                 method: 'POST',
                 body: JSON.stringify(requestData),
@@ -27,21 +26,22 @@ function sendVisitTimePeriodically() {
                     'X-CSRFToken': 'csrftoken'
                 }
             })
-                .then(response => {
+                .then((response) => {
                 if (!response.ok) {
                     throw new Error('Network response was not ok');
                 }
                 return response.json();
             })
-                .then(responseData => {
+                .then((responseData) => {
                 console.log('Ответ сервера:', responseData);
-                saveVisitTime(); // обновляем время после успешной отправки запроса
+                // Обновляем только visitTime, не перезаписывая startTime
+                localStorage.setItem('visitTime', currentTime.toString());
             })
-                .catch(error => {
+                .catch((error) => {
                 console.error('Произошла ошибка:', error);
             });
         }
-    }, 10000); // Отправляем данные каждые 10 секунд (в миллисекундах)
+    }, 10000);
 }
 saveVisitTime();
 sendVisitTimePeriodically();
--- dezor/static/ts/scripts.ts
+++ dezor/static/ts/scripts.ts
@@ -1,7 +1,7 @@
 let startTime: number = 0
 
 function saveVisitTime (): void {
-  startTime = Math.floor(new Date().getTime() / 1000) // конвертируем время начала в секунды
+  startTime = Math.floor(new Date().getTime() / 1000)
   localStorage.setItem('visitTime', startTime.toString())
 }
 
@@ -9,8 +9,8 @@ function sendVisitTimePeriodically (): void {
   setInterval(() => {
     const visitTime: string | null = localStorage.getItem('visitTime')
     if (visitTime) {
-      const currentTime: number = Math.floor(new Date().getTime() / 1000) // текущее время в секундах
-      const elapsedTime: number = currentTime - parseInt(visitTime) // вычисляем прошедшее время в секундах
+      const currentTime: number = Math.floor(new Date().getTime() / 1000)
+      const elapsedTime: number = currentTime - parseInt(visitTime)
 
       const requestData = {
         id: localStorage.getItem('userId'),
@@ -21,7 +21,7 @@ function sendVisitTimePeriodically (): void {
 
       const currentDomain: string = window.location.origin
       const url: string = `${currentDomain}/submit_data/`
-      // Отправляем данные на сервер
+
       fetch(url, {
         method: 'POST',
         body: JSON.stringify(requestData),
@@ -30,23 +30,23 @@ function sendVisitTimePeriodically (): void {
           'X-CSRFToken': 'csrftoken'
         }
       })
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok')
-        }
-        return response.json()
-      })
-      .then(responseData => {
-        console.log('Ответ сервера:', responseData)
-        saveVisitTime() // обновляем время после успешной отправки запроса
-      })
-      .catch(error => {
-        console.error('Произошла ошибка:', error)
-      })
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error('Network response was not ok')
+          }
+          return response.json()
+        })
+        .then((responseData) => {
+          console.log('Ответ сервера:', responseData)
+          // Обновляем только visitTime, не перезаписывая startTime
+          localStorage.setItem('visitTime', currentTime.toString())
+        })
+        .catch((error) => {
+          console.error('Произошла ошибка:', error)
+        })
     }
-  }, 10000) // Отправляем данные каждые 10 секунд (в миллисекундах)
+  }, 10000)
 }
 
 saveVisitTime()
 sendVisitTimePeriodically()
-
